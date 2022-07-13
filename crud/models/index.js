c22ff0d2c310b54d9206e14ed3794be243db3c8f
kimgyuha <kimgyuha123@naver.com>
2022-07-13 12:35:58 +0900

    Create CRUD index models

---
+++ crud/models/index.js
@@ -0,0 +1,20 @@
+'use strict'; //엄격모드 적용
+
+const Sequelize = require('sequelize');
+const env = process.env.NODE_ENV || 'development';
+const config = require('../config/config.json')[env];
+const db = {};
+
+const sequelize = new Sequelize(
+  config.database,
+  config.username,
+  config.password,
+  config
+);
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.Posts = require("./posts")(sequelize, Sequelize);
+
+module.exports = db;
