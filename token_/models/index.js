bb00069be9cb2c3e0a8d36784aa909df98d1ba0c
kimgyuha <kimgyuha123@naver.com>
2022-07-13 12:39:20 +0900

    Create token index models

---
+++ token_/models/index.js
@@ -0,0 +1,14 @@
+const  Sequelize = require("sequelize")
+const env = process.env.NODE_ENV || 'development';
+const config = require("../config/config.json")[env];
+
+const db = {};
+
+const sequelize = new Sequelize({...config, sync : true});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+db.User = require("./user")(sequelize, Sequelize);
+
+module.exports = db;
